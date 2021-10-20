da38cc0a8d6acbf39326bad7dc006fff5973b4e4
Noel Nyende <72074456+neo256-star@users.noreply.github.com>
2021-10-20 11:14:51 +0300

    Update chmembers.h

--- chmembers.h
+++ chmembers.h
@@ -5,54 +5,64 @@
 #ifndef CHESS01_CHMEMBERS_H
 #define CHESS01_CHMEMBERS_H
 
-// #include "headers.h"
+//#include "headers.h"
 
-class ChMembers;  //define appropriately
+/* rules functions */
+void white_pawn_rules();
+void white_knight_rules();
+void white_rook_rules();
+void white_bishop_rules();
+void white_queen_rules();
+void white_king_rules();
+void black_pawn_rules();
+void black_knight_rules();
+void black_ rook_rules();
+void black_bishop_rules();
+void black_queen_rules();
+void black_ king_rules();
+/*rules functions declarations */
 
-namespace ChPieces{
-
-	namespace amount{}
-}
-namespace white{
-	enum class pawn{
-		pawn1,pawn2,pawn3,pawn4,pawn5,pawn6,pawn7,pawn8
-	};
-	enum class knight{
-		knight1,knight2
-	};
-	enum class rook{
-		rook1,rook2
-	};
-	enum class bishop{
-		bishop1,bishop2
-	};
-	enum class queen{
-		queen1  //1 at end of definition because unlike a king a queen,
-		        // isn't constantly one
-	};
-	enum class king{
-		king
-	};
-}
-namespace black{
-	enum class pawn{
-		pawn1,pawn2,pawn3,pawn4,pawn5,pawn6,pawn7,pawn8
-	};
-	enum class knight{
-		knight1,knight2
-	};
-	enum class rook{
-		rook1,rook2
-	};
-	enum class bishop{
-		bishop1,bishop2
-	};
-	enum class queen{
-		queen1  //1 at end of definition because unlike a king a queen,
+namespace chmembers{
+	namespace white{
+		enum class pawn{
+			pawn1,pawn2,pawn3,pawn4,pawn5,pawn6,pawn7,pawn8
+		}  //pawn
+		enum class knight{
+			knight1,knight2
+		}//knight
+		enum class rook{
+			rook1,rook2
+		}//rook
+		enum class bishop{
+			bishop1,bishop2
+		}//bishop
+		enum class queen{
+			queen1
+		}//queen
+		enum class king{
+			king
+		}//king
+	} // white
+	namespace black{
+		enum class pawn{
+			pawn1,pawn2,pawn3,pawn4,pawn5,pawn6,pawn7,pawn8
+		}  //pawn
+		enum class knight{
+			knight1,knight2
+		}//knight
+		enum class rook{
+			rook1,rook2
+		}//rook
+		enum class bishop{
+			bishop1,bishop2
+		}//bishop
+		enum class queen{
+			queen1   //1 at end of definition because unlike a king a queen
 		// isn't constantly one
-	};
-	enum class king{
-		king
-	};
-}
+		}//queen
+		enum class king{
+			king
+		}//king
+	}  //black
+} //chmembers
 #endif //CHESS01_CHMEMBERS_H
